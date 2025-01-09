da708a6bf8f7ab825560911e5d5140a7c547cd79
nepluz <zeynepkoroglu2002@hotmail.com>
2025-01-09 10:47:51 +0100

    cheese anim script

--- Assets/animations/Cheese/Cheese_Animations.cs
+++ Assets/animations/Cheese/Cheese_Animations.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Cheese_Animations : MonoBehaviour
 {
     public Animator animator;
+    public void Cheese_Anim_1()
+    {
+        animator.SetBool("Cheese_Anim", true);
+    }
+    public void SeeCheese_2()
+    {
+        animator.SetBool("Cheese_Anim", false);
+    }
 }
